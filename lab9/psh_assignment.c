9ea343998814f022b54649c786057781e0427bc4
Ubuntu <akamono312>
2022-11-02 19:28:36 +0000

    assignment

--- lab9/psh_assignment.c
+++ lab9/psh_assignment.c
@@ -2,32 +2,48 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
-void init_arglist(char* *arglist[10]) {
+
+void fork_program(int ppid) {
+    char *arglist[10];
+    char buf[100];
+    int status;
+    int fork_rv;
     int i;
-    for(i = 0; i < 10; i++) {
-        arglist[i] = NULL;
+    i = 0;
+
+    fork_rv = fork();
+    if(fork_rv == -1) {
+        perror("fork");
+    } else if(fork_rv == 0) {
+        while(i < 10) {
+            printf("Wooseok Arg[%d]? ", i);
+            gets(buf);
+            if(strcmp(buf, "quit") == 0)  {
+                kill(ppid, SIGINT);
+                exit(1);
+            }
+            arglist[i] = (char*)malloc(strlen(buf) + 1);
+            strcpy(arglist[i], buf);
+            if (strcmp(arglist[i], "") == 0) {
+                arglist[i] = NULL;
+                execvp(arglist[0], arglist);
+                exit(1);
+            }
+            i++;
+        }
+    } else {
+        wait(&status);
+        fork_program(ppid);
     }
 }
 
+
 int main() {
-    char *arglist[10];
-    char buf[100];
-    int i;
-    i = 0;
-    while(i < 10) {
-        printf("Wooseok Arg[%d]? ", i);
-        gets(buf);
-        if(strcmp(buf, "-1") == 0) break;
-        arglist[i] = (char*) malloc(strlen(buf) + 1);
-        strcpy(arglist[i], buf);
-        if (strcmp(arglist[i], "") == 0) {
-            arglist[i] = NULL;
-            execvp(arglist[0], arglist);
-            init_arglist(&arglist);
-            i = -1;
-        }
-        i++;
-    }
-    execvp(arglist[0], arglist);
+    printf("Input \"quit\" to quit the program\n");
+    int parentPid = getpid();
+    fork_program(parentPid);
+
+    return 0;
 }
